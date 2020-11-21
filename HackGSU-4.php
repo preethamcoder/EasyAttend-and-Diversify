33010d227ac2dca885307d871e29264d126cdb1e
nachikethinge01 <55408263+nachikethinge01@users.noreply.github.com>
2020-11-21 14:13:09 -0800

    Add files via upload

---
+++ HackGSU-4.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, intial-scale = 1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <style>
+        body {
+            background-color: #7e53f3;
+        }
+
+        h1 {
+            color: white;
+            position: absolute;
+            margin-left: 33%;
+            margin-top: 22%;
+            font-size: 3.0vw;
+        }
+
+        img {
+            width: 15%;
+            position: absolute;
+            margin-left: 42%;
+            margin-top: 2%;
+        }
+
+        a {
+            text-decoration: none;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>YOU HAVE ALREADY BEEN CHECKED IN!</h1>
+    <a href="HackGSUv1" Method="GET" class="home">Go Back</a>
+    <div class="work">
+        <a href="#">
+            <center>ID Entered! <br><br> Course Select: Mathematics <br> Mathematics: Adding 1s and 10s</center>
+        </a>
+    </div>
+
+</body>
