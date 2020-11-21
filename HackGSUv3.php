42d12fadf6a15d9665cfb63d8a42283752a96fa9
preethampython101 <46249739+preethampython101@users.noreply.github.com>
2020-11-21 10:33:04 -0500

    Add files via upload

---
+++ HackGSUv3.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html> 
+</head>
+
+    <body>
+       
+        <h1>YOU DON'T EXIST!!</h1>
+        <a href="HackGSUv1" Method="GET" class="home">Go Back</a>
+        <div class="work">
+            <a href="#"><center>ID Entered! <br><br> Course Select: Mathematics <br> Mathematics: Adding 1s and 10s</center> </a>
+        </div>
+
+
+    </body>
+</html>
