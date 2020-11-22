18b978defe7067122615f3668281a06b93b9b976
nachikethinge01 <55408263+nachikethinge01@users.noreply.github.com>
2020-11-22 07:50:50 -0800

    Update HackGSUv8_temp.php

--- HackGSUv8_temp.php
+++ HackGSUv8_temp.php
@@ -1,21 +1,76 @@
+<html>
 
-        <html>
-        <head>
-        <title>Attendance for ECON2106 </title>
-        </head>
-        <body>
-        <table border="2" cellspacing="2" cellpadding="2">
-        <tr>
-        <th><font face="Arial,Helvetica,sans-serif">ID</font></th>
-        <th><font face="Arial,Helvetica,sans-serif">First Name</font></th>
-        <th><font face="Arial,Helvetica,sans-serif">Last Name</font></th>
-        </tr>
-        <tr>
-            <td><font face="Arial, Helvetica, sans-serif">1234554321</font></td>
-            <td><font face="Arial, Helvetica, sans-serif">Shah Rukh</font></td>
-            <td><font face="Arial, Helvetica, sans-serif">Khan</font></td>
-            </tr>
-            </table>
-                       </body>
-                       </html>
-                    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, intial-scale = 1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        body {
+            background: url("https://github.com/preethampython101/Hackathon2020/blob/main/Img-1.jpg?raw=true");
+            background-repeat: no-repeat;
+            background-size: 100%;
+        }
+
+        h1 {
+            color: white;
+            position: absolute;
+            margin-left: 40%;
+            margin-top: 3%;
+            font-size: 3.0vw;
+        }
+
+        img {
+            width: 15%;
+            position: absolute;
+            margin-left: 42%;
+            margin-top: 2%;
+        }
+
+        a {
+            display: inline-block;
+            padding: 10px 20px;
+            width: 15%;
+            margin-top: 25%;
+            margin-left: 40%;
+            padding: 2.0%;
+            color: #ebebeb;
+            font-size: 2.5vw;
+            font-family: Tahoma, Geneva, sans-serif;
+            text-align: center;
+            text-decoration: none;
+            background: #c78cfa;
+            border-radius: 15px;
+            box-shadow: 10px 10px 10px #7228bd,
+                -5px -5px 12px #9c4ced;
+            text-shadow:
+                0px 1px 3px rgba(000, 000, 000, 0.3),
+                0px 0px 0px rgba(255, 255, 255, 0);
+            position: absolute;
+            clear: both;
+            border: none;
+            font-weight: strong;
+        }
+
+        a:hover {
+            box-shadow: inset -3px -3px 3px rgba(255, 255, 255, 0.5),
+                inset 5px 5px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        table{
+            margin-top: 8%;
+            margin-left: 37%;
+            position: absolute;
+            background-color: #c78cfa;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Attendance</h1>
+    <table border="2" cellspacing="2" cellpadding="2">
+  
+    </table>
+    <a href="HackGSUv1" Method="GET" class="home">Go Back</a>
+</body>
+
+</html>
