f07091b8926026576e51696984df4bea60e1624b
preethampython101 <46249739+preethampython101@users.noreply.github.com>
2020-11-22 10:39:38 -0500

    Add files via upload

---
+++ HackGSUv8_temp.php
@@ -0,0 +1,21 @@
+
+        <html>
+        <head>
+        <title>Attendance for ECON2106 </title>
+        </head>
+        <body>
+        <table border="2" cellspacing="2" cellpadding="2">
+        <tr>
+        <th><font face="Arial,Helvetica,sans-serif">ID</font></th>
+        <th><font face="Arial,Helvetica,sans-serif">First Name</font></th>
+        <th><font face="Arial,Helvetica,sans-serif">Last Name</font></th>
+        </tr>
+        <tr>
+            <td><font face="Arial, Helvetica, sans-serif">1234554321</font></td>
+            <td><font face="Arial, Helvetica, sans-serif">Shah Rukh</font></td>
+            <td><font face="Arial, Helvetica, sans-serif">Khan</font></td>
+            </tr>
+            </table>
+                       </body>
+                       </html>
+                    
