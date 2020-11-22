0b06fba0c2adcc277510c2bc746168e100fb1ec6
Hakan Can Gunerli <hakancangunerli@gmail.com>
2020-11-21 19:35:43 -0500

    added the python thing

--- GE challenge/sort.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-csv_file = pd.read_csv('students.csv')
-sorted_race = csv_file.sort_values(by=['Race'])
-df = pd.read_csv (r'students.csv')
-#add so that it sorts only certain race, 
-
