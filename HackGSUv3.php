ef40f7252cc9db4c7cc1ad22319410ab38b7f2df
preethampython101 <46249739+preethampython101@users.noreply.github.com>
2020-11-21 20:00:40 -0500

    Update HackGSUv3.php

--- HackGSUv3.php
+++ HackGSUv3.php
@@ -5,12 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width-device-width, intial-scale = 1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     <style>
         body {
             background-color: #7e53f3;
         }
-
         h1 {
             color: white;
             position: absolute;
@@ -18,21 +16,18 @@
             margin-top: 22%;
             font-size: 3.0vw;
         }
-
         img {
             width: 15%;
             position: absolute;
             margin-left: 42%;
             margin-top: 2%;
         }
-
         a {
             text-decoration: none;
             color: white;
         }
     </style>
 </head>
-
 <body>
     <h1>YOU DON'T EXIST!!</h1>
     <a href="HackGSUv1" Method="GET" class="home">Go Back</a>
@@ -41,6 +36,5 @@
             <center>ID Entered! <br><br> Course Select: Mathematics <br> Mathematics: Adding 1s and 10s</center>
         </a>
     </div>
-
 </body>
 </html>
