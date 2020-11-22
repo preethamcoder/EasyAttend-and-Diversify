8a2f9a854d5d683601c9f8456ceb20b810a13b6e
nachikethinge01 <55408263+nachikethinge01@users.noreply.github.com>
2020-11-21 17:20:06 -0800

    Update HackGSU-4.php

--- HackGSU-4.php
+++ HackGSU-4.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width-device-width, intial-scale = 1.0">
@@ -8,6 +9,7 @@
         body {
             background-color: #7e53f3;
         }
+
         h1 {
             color: white;
             position: absolute;
@@ -15,25 +17,49 @@
             margin-top: 22%;
             font-size: 3.0vw;
         }
+
         img {
             width: 15%;
             position: absolute;
             margin-left: 42%;
             margin-top: 2%;
         }
+
         a {
+            display: inline-block;
+            padding: 10px 20px;
+            width: 20%;
+            margin-top: 27%;
+            margin-left: 37%;
+            padding: 2.5%;
+            color: #ebebeb;
+            font-size: 1.5vw;
+            font-family: Tahoma, Geneva, sans-serif;
+            text-align: center;
             text-decoration: none;
-            color: white;
+            background: #c78cfa;
+            border-radius: 15px;
+            box-shadow: 10px 10px 10px #7228bd,
+                -5px -5px 12px #9c4ced;
+            text-shadow:
+                0px 1px 3px rgba(000, 000, 000, 0.3),
+                0px 0px 0px rgba(255, 255, 255, 0);
+            position: absolute;
+            clear: both;
+            border: none;
+            font-weight: strong;
+        }
+
+        a:hover {
+            box-shadow: inset -3px -3px 3px rgba(255, 255, 255, 0.5),
+                inset 5px 5px 5px rgba(0, 0, 0, 0.1);
         }
     </style>
 </head>
+
 <body>
-    <h1>YOU HAVE ALREADY BEEN CHECKED IN!</h1>
+    <h1>You Checked In Earlier!</h1>
     <a href="HackGSUv1" Method="GET" class="home">Go Back</a>
-    <div class="work">
-        <a href="#">
-            <center>ID Entered! <br><br> Course Select: Mathematics <br> Mathematics: Adding 1s and 10s</center>
-        </a>
-    </div>
 </body>
+
 </html>
