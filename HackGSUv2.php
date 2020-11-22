1fd8435fe8f08c495dd75d1b30eaf0883d65d7ff
preethampython101 <46249739+preethampython101@users.noreply.github.com>
2020-11-21 20:00:20 -0500

    Update HackGSUv2.php

--- HackGSUv2.php
+++ HackGSUv2.php
@@ -5,12 +5,10 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width-device-width, intial-scale = 1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
         <style>
             body {
                 background-color: #7e53f3;
             }
-
             h1 {
                 color: white;
                 position: absolute;
@@ -18,27 +16,20 @@
                 margin-top: 22%;
                 font-size: 3.0vw;
             }
-
             img {
                 width: 15%;
                 position: absolute;
                 margin-left: 42%;
                 margin-top: 2%;
             }
-
             a{
                 text-decoration: none;
                 color: white;
             }
-
-
         </style>
     </head>
-
     <body>
         <center><h1>Welcome! You have been checked in!</h1></center>
         <a href="HackGSUv1" Method="GET" class="home">Go Back</a>
-
-
     </body>
 </html>
